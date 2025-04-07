599aea1bda09113634015574b40e16f45f48295b
kokonutw <castaneda.jair.13@gmail.com>
2025-04-06 19:17:15 -0500

    fix(apps/client): url api

--- apps/client/src/redux/services/base-api.js
+++ apps/client/src/redux/services/base-api.js
@@ -2,7 +2,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { fetchBaseWithAuth } from "../fetchBaseWithAuth";
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseWithAuth(`${import.meta.env.VITE_API_URL}`),
+  baseQuery: fetchBaseWithAuth(`${import.meta.env.VITE_API_URL}/api`),
   reducerPath: "baseApi",
   tagTypes: ["Pins", "Collections"],
   endpoints: () => ({}),

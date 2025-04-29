2a4e470e7bb5d6b451e3a5593e1da3950ce344d5
kokonut <castaneda.jair.13@gmail.com>
2025-04-29 06:20:36 -0500

    refactor(apps/api): number(id)

--- apps/api/src/controllers/pines-controller.js
+++ apps/api/src/controllers/pines-controller.js
@@ -95,7 +95,7 @@ export const deletePin = async (req, res) => {
   const { pinId } = req.params;
 
   try {
-    const deleted = await deletePinsById(pinId);
+    const deleted = await deletePinsById(Number(pinId));
 
     return res.status(200).json({ message: "pin deleted", pin: deleted });
   } catch (error) {
